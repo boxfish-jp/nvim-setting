ac705d9f6e2e8bbc343a8461e5546ba5c967cd15
labfish <labfish893@gmail.com>
2024-11-11 16:19:28 +0900

    feat: copilot-cmpの有効と無効トグルを追加。cmpのソート順番やろうとしたが、copilotが下にならなかった。 / #1

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,6 +17,34 @@ return {
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp", group_index = 1 },
+          { name = "buffer", group_index = 1 },
+          { name = "luasnip", group_index = 2 },
+          { name = "path", group_index = 1 },
+          { name = "copilot", group_index = 3 },
+        },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            -- require("copilot_cmp.comparators").prioritize,
+
+            -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            --      cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+      })
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
@@ -40,10 +68,26 @@ return {
         end, { "i", "s" }),
         ["<CR>"] = cmp.config.disable,
       })
-      table.insert(opts.sources, 1, {
-        name = "copilot",
-        group_index = 2,
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    opts = function()
+      local copilot_toggle = require("lazyvim.util.toggle").wrap({
+        name = "Copilot Completion",
+        get = function()
+          return not require("copilot.client").is_disabled()
+        end,
+        set = function(state)
+          if state then
+            require("copilot.command").enable()
+          else
+            require("copilot.command").disable()
+          end
+        end,
       })
+
+      LazyVim.toggle.map("<leader>at", copilot_toggle)
     end,
   },
 }
