d925735754f6dc3a80e156e46242d8622383d28e
labfish <labfish893@gmail.com>
2024-11-13 17:31:03 +0900

    feat: クリップボードとファイルの内容を比較するコマンドを追加

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+function Close_diff_buffers() end
+
+local function compare_to_clipboard()
+  local ftype = vim.api.nvim_eval("&filetype")
+  vim.cmd(string.format(
+    [[
+    vsplit
+    wincmd l
+    enew
+    setlocal buftype=nofile | set filetype=%s
+    normal! P
+    diffthis
+    wincmd h | diffthis
+  ]],
+    ftype
+  ))
+end
+
+vim.keymap.set("n", "<leader>bc", compare_to_clipboard, { desc = "Compare to clipboard" })
