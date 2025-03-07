6239b04b3996d6ccb017d07af154d4f87be29337
boxfish_jp <boxfish893@gmail.com>
2025-03-07 18:09:31 +0900

    fix: biomeが動かない問題を修正

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -3,12 +3,12 @@ return {
     "stevearc/conform.nvim",
     opts = function(_, opts)
       opts.formatters_by_ft = {
-        ["javascript"] = { "biome check" },
-        ["javascriptreact"] = { "biome check" },
-        ["typescript"] = { "biome check" },
-        ["typescriptreact"] = { "biome check" },
-        ["json"] = { "biome check" },
-        ["css"] = { "biome check" },
+        ["javascript"] = { "biome-check" },
+        ["javascriptreact"] = { "biome-check" },
+        ["typescript"] = { "biome-check" },
+        ["typescriptreact"] = { "biome-check" },
+        ["json"] = { "biome-check" },
+        ["css"] = { "biome-check" },
       }
       return opts
     end,
