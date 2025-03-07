4c3f43b75c8e1a1f99a01ea0a06f46e420c591f1
boxfish_jp <boxfish893@gmail.com>
2025-03-08 01:14:50 +0900

    feat: pythonのformatterをruffにした

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -9,6 +9,14 @@ return {
         ["typescriptreact"] = { "biome-check" },
         ["json"] = { "biome-check" },
         ["css"] = { "biome-check" },
+        ["python"] = {
+          -- To fix auto-fixable lint errors.
+          "ruff_fix",
+          -- To run the Ruff formatter.
+          "ruff_format",
+          -- To organize the imports.
+          "ruff_organize_imports",
+        },
       }
       return opts
     end,
