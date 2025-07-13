aa4ca2d8842a339372a94dfa23d5937f3161aa55
boxfish_jp <boxfish893@gmail.com>
2025-07-14 00:42:06 +0900

    feat: codeとexplorerを開くコマンドを追加

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.o.wrap = true
+
+vim.api.nvim_create_user_command('Code', function()
+  vim.system({ "code", "." })
+end, {})
+vim.api.nvim_create_user_command('Explorer', function()
+  vim.system({ "explorer", "." })
+end, {})
